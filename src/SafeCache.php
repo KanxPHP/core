0faee0c2ea9afd54958800a107adc840194c627d
Marco Monteiro <kanxuk@gmail.com>
2026-05-01 12:28:26 +0100

    Updated SafeCache and added new kanx-cli command cache:clear

--- src/SafeCache.php
+++ src/SafeCache.php
@@ -4,14 +4,19 @@ namespace KanxPHP\Core;
 
 class SafeCache 
 {
-    private static string $storage = __DIR__ . '/../../storage/cache';
+    private static string $storagePath = '/../storage/cache';
+    private static string $storage = '';
 
     /**
      * Store data using the OpCache-friendly "var_export" method.
      */
     public static function set(string $key, $val, string $timeout = '+24 hours'): void 
     {
-        if (!is_dir(self::$storage)) mkdir(self::$storage, 0755, true);
+        self::$storage = getcwd() . self::$storagePath;
+     
+        if (!is_dir(self::$storage)) {
+            mkdir(self::$storage, 0755, true);
+        }
 
         $expire = strtotime($timeout);
         $expireExport = var_export($expire, true);
@@ -22,7 +27,7 @@ class SafeCache
         
         $content = "<?php \$expire = $expireExport; \$val = $valExport;";
         $file = self::$storage . '/' . md5($key) . '.php';
-        
+   
         // Atomic write
         file_put_contents($file . '.tmp', $content, LOCK_EX);
         rename($file . '.tmp', $file);
@@ -33,6 +38,8 @@ class SafeCache
      */
     public static function get(string $key) 
     {
+        self::$storage = getcwd() . self::$storagePath;
+     
         $file = self::$storage . '/' . md5($key) . '.php';
 
         if (!file_exists($file)) return null;
